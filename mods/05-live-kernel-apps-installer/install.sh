75d0be44c036d769d65d9f49886d67e32450e6da
Anduin Xue <anduin@aiursoft.com>
2026-06-07 16:03:48 +0000

    feat: enhance anduinos-desktop installation by adding missing packages

--- mods/05-live-kernel-apps-installer/install.sh
+++ mods/05-live-kernel-apps-installer/install.sh
@@ -25,9 +25,23 @@ print_ok "Installing anduinos-desktop (full AnduinOS desktop metapackage)..."
 apt install $INTERACTIVE \
     anduinos-desktop \
     anduinos-desktop-apps \
+    anduinos-gnome-extensions \
     anduinos-appstore \
     anduinos-theme \
     anduinos-wallpapers \
+    anduinos-fonts \
+    anduinos-no-snapd \
+    anduinos-session \
+    anduinos-software-properties-common \
+    anduinos-software-properties-gtk \
+    anduinos-system-tweaks \
+    firefox-anduinos \
+    gnome-shell-extension-appindicator-anduinos \
+    gnome-shell-extension-dash-to-panel-anduinos \
+    gnome-shell-extension-desktop-icons-ng-anduinos \
+    plymouth-anduinos \
+    alsa-ucm-conf-anduinos \
+    firmware-sof-anduinos \
     --install-recommends
 judge "Install anduinos-desktop"
 
