3adc0d9c1fd81cc7b3b090f6a2ceecd8821a1b3a
Manoj nandanwar <manojnitbhu2@gmail.com>
2026-08-04 15:40:37 +0530

    fix: move autoinstall ds=nocloud before --- in KERNEL_APPEND (#167)

--- tools/vm/install-current-iso.sh
+++ tools/vm/install-current-iso.sh
@@ -94,7 +94,7 @@ KERNEL_OUT=$(bash "$(dirname "$0")/extract-installer-kernel.sh" \
 
 VMLINUZ=$(python3 -c "import json; d=json.load(open('$KERNEL_JSON')); print(d['vmlinuz_path'])")
 INITRD=$(python3 -c "import json; d=json.load(open('$KERNEL_JSON')); print(d['initrd_path'])")
-KERNEL_APPEND="boot=casper locale=en_US.UTF-8 quiet splash console=ttyS0,115200n8 init=/lib/systemd/systemd --- autoinstall ds=nocloud"
+KERNEL_APPEND="boot=casper autoinstall ds=nocloud locale=en_US.UTF-8 quiet splash console=ttyS0,115200n8 init=/lib/systemd/systemd ---"
 
 printf '[INFO] Booting 0.3.0 ISO in %s mode as managed background VM (token: %s, Port: %s)...\n' "$MODE" "$INSTALL_TOKEN" "$SSH_PORT"
 
